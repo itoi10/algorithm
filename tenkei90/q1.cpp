57ca31a3076b6bcb8998a6c9cdd7317ad575ba04
itict10 <itict10@gmail.com>
2022-05-01 07:28:26 +0900

    tenkei 001

---
+++ tenkei90/q1.cpp
@@ -0,0 +1,59 @@
+/*
+典型 001 https://atcoder.jp/contests/typical90/tasks/typical90_a
+
+g++ q1.cpp; ./a.out < input.txt
+
+入力例
+---
+3 34
+1
+8 13 26
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Lを長さM以上のピースに分解できるか判定
+bool solve(int M, int N, int L, int K, vector<int> A) {
+    int cnt = 0;
+    int pre = 0;
+    for (int i=0; i<N; i++) {
+        if(A.at(i) - pre >= M && L - A.at(i) >= M) {
+            cnt += 1;
+            pre = A.at(i);
+        }
+    }
+    if (cnt >= K) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    // 入力値
+    int N, L, K;
+    cin >> N >> L >> K;
+    vector<int> A(N);
+    for (int i=0; i<N; i++) {
+        cin >> A.at(i);
+    }
+
+    // 二分法
+    int left = -1;
+    int right = L + 1;
+
+    while (right - left > 1) {
+        int mid = left + (right - left) / 2;
+        if (solve(mid, N, L, K, A)) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+    
+    // 解
+    cout << left << endl;
+
+    return 0;
+}
---
+++ tenkei90/q1.py
@@ -0,0 +1,46 @@
+"""
+典型 001 https://atcoder.jp/contests/typical90/tasks/typical90_a
+python q1.py < input.txt
+"""
+N, L = map(int, input().split())
+K = int(input())
+A = list(map(int, input().split()))
+
+
+def solve(M: int) -> bool:
+    """Lを長さM以上のピースに分解できるか判定"""
+    # 分割回数
+    cnt = 0
+    # 前回区切った位置
+    pre = 0
+
+    for i in range(N):
+        # 区切りの左右がどちらも長さM以上なら分割可能
+        if (A[i] - pre >= M) and (L - A[i] >= M):
+            cnt += 1
+            pre = A[i]
+
+    # K回で分割可能
+    if cnt >= K:
+        return True
+    return False
+
+
+# 解を二分法で求める
+# cf. [二分探索アルゴリズムを一般化 〜 めぐる式二分探索法のススメ 〜](https://qiita.com/drken/items/97e37dd6143e33a64c8c)
+left = -1
+right = L + 1
+
+while right - left > 1:
+    # 中間値
+    mid = int(left + (right - left) / 2)
+    # 中間値で分割可能?
+    if solve(mid):
+        # 右に範囲を狭める
+        left = mid
+    else:
+        # 左に範囲を狭める
+        right = mid
+
+# 解
+print(left)

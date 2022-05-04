9bb3d92d433c4d0ce9a4a5f52e81e4a391425050
itict10 <itict10@gmail.com>
2022-05-04 20:50:28 +0900

    tenkei 24

---
+++ tenkei90/q24.py
@@ -0,0 +1,15 @@
+"""
+典型 024 https://atcoder.jp/contests/typical90/tasks/typical90_x
+"""
+
+N, K = map(int, input().split())
+A = map(int, input().split())
+B = map(int, input().split())
+
+dif = sum([abs(a - b) for a, b in zip(A, B)])
+
+# K回に間に合って余分が偶数回ならYes
+if dif <= K and (K - dif) % 2 ==0:
+    print("Yes")
+else:
+    print("No")

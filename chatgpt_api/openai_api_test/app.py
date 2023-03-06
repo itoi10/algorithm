40d003dfa7a6b17a5c4878ce5adb67401ab84f62
itict10 <itict10@gmail.com>
2023-03-06 20:48:12 +0900

    front and api integration

--- chatgpt_api/openai_api_test/app.py
+++ chatgpt_api/openai_api_test/app.py
@@ -1,26 +1,28 @@
-from chalice import Chalice
+from chalice import Chalice, CORSConfig
 import openai
 
 app = Chalice(app_name="openai_api_test")
 
+cors_config = CORSConfig(allow_origin="*")
 
-@app.route("/", methods=["POST"])
+
+@app.route("/", methods=["POST"], cors=cors_config)
 def index():
     """
     受け取るJSON
-    [
+    { "messages": [
         {"role": "user", "content": "こんにちは"},
         {"role": "assistant", "content": "こんにちは！私はAIアシスタントです。何かお手伝いできることがありますか？"},
         {"role": "user", "content": "次のメッセージ"},
-    ]
+    ]}
 
     例
-    $ curl -XPOST <デプロイしたURL> -H "Content-Type: application/json" -d '[{"role":"user","content":"こんにちは"}]'
+    $ curl -XPOST <デプロイしたURL> -H "Content-Type: application/json" -d '{"messages":[{"role":"user","content":"こんにちは"}]}'
     """
 
     try:
         # JSON読み込み
-        messages = app.current_request.json_body
+        messages = app.current_request.json_body["messages"]
 
         # ChatGPTに送信
         response = openai.ChatCompletion.create(
